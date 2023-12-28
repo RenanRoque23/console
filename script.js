d7c366b3918b284b0dbf4ee14d8d7f98324e4dfb
RenanRoque23 <148238527+RenanRoque23@users.noreply.github.com>
2023-12-28 11:11:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("meu primeiro projeto no console");
+
+           var firtName = "Renan ";
+           var lastName = "Roque"
+        
+           var fullName = firtName.concat(lastName);
+           console.log(fullName);
+
+           var a = 10;
+           var b = 5;
+           
+           var add = a + b;
+           console.log(add)
+           
+           var sub = a - b;
+           console.log(sub)
+
+           var times = a * b;
+           console.log(times)
+
+           var divide = a / b;
+           console.log(divide)
+
